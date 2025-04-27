6432037d92eb540682f7fd51ce267dc86b298997
Avsian <aavsian3@heck-srv2.ece.gatech.edu>
2025-04-27 10:07:19 -0400

    rand and len results

--- exp.sh
+++ exp.sh
@@ -1,15 +1,27 @@
-PROPORTION=1.0
-METHOD="rand"
+PROPORTION=.2
+METHOD="len"
 NAME="${METHOD}_${PROPORTION}"
 
-# rm -rf models/"$NAME"
+rm -rf models/"$NAME"
 accelerate launch \
   --multi_gpu \
   --num_processes 8 \
   train.py \
     --proportion $PROPORTION \
-    --method $RAND \
+    --method $METHOD \
     --name $NAME
 
 python inference.py --name $NAME
 python eval.py --name $NAME
+
+# python eval.py --name rand_.05
+# python eval.py --name rand_.2
+# python eval.py --name rand_.4
+# python eval.py --name rand_.6
+# python eval.py --name rand_.8
+# python eval.py --name rand_1.0
+# python eval.py --name len_.05
+# python eval.py --name len_.2
+# python eval.py --name len_.4
+# python eval.py --name len_.6
+# python eval.py --name len_.8
